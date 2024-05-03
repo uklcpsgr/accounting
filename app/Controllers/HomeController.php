0c6216fa3fe16319321a827b8c92c18a20a43f08
Oleh V <oleg.vlakhno@gmail.com>
2024-05-03 12:05:16 +0300

    Add very basic DI container.

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,12 +4,16 @@ declare(strict_types = 1);
 
 namespace App\Controllers;
 
+use App\App;
+use App\Services\InvoiceService;
 use App\View;
 
 class HomeController
 {
     public function index(): View
     {
+        App::$container->get(InvoiceService::class)->process([], 25);
+
         return View::make('index');
     }
 }
