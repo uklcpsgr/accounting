5f80cf8eccaebcb1c05ad92b590fb2d968ffc696
Oleh V <oleg.vlakhno@gmail.com>
2024-05-05 19:43:42 +0300

    Implenet interface support in DI container.

--- app/Container.php
+++ app/Container.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace App;
 
 use App\Exceptions\Container\ContainerException;
-use App\Exceptions\Container\NotFoundException;
 use Psr\Container\ContainerInterface;
 
 class Container implements ContainerInterface
@@ -17,7 +16,10 @@ class Container implements ContainerInterface
         if ($this->has($id)) {
             $entry = $this->entries[$id];
 
-            return $entry($this);
+            if (is_callable($entry)) {
+                return $entry($this);
+            }
+            $id = $entry;
         }
 
         return $this->resolve($id);
@@ -29,7 +31,7 @@ class Container implements ContainerInterface
         return isset($this->entries[$id]);
     }
 
-    public function set(string $id, callable $concrete)
+    public function set(string $id, callable|string $concrete)
     {
         $this->entries[$id] = $concrete;
     }
