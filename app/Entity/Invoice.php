9e4eac0e7266ca0361b77ab3f0db7513a4ecbe69
Oleh V <oleg.vlakhno@gmail.com>
2024-05-09 16:00:30 +0300

    Add doctrine and invoice entity.

---
+++ app/Entity/Invoice.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enums\InvoiceStatus;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity]
+#[Table('invoices')]
+class Invoice
+{
+    #[Id]
+    #[Column, GeneratedValue]
+    private int $id;
+
+    #[Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private float $amount;
+
+    #[Column(name: 'invoice_number')]
+    private string $invoiceNumber;
+
+    #[Column]
+    private InvoiceStatus $status;
+
+    #[Column(name: 'created_at')]
+    private \DateTime $createdAt;
+
+    #[OneToMany(targetEntity: InvoiceItem::class, mappedBy: 'invoice')]
+    private Collection $items;
+
+    public function __constuctor()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): Invoice
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function getInvoiceNumber(): string
+    {
+        return $this->invoiceNumber;
+    }
+
+    public function setInvoiceNumber(string $invoiceNumber): Invoice
+    {
+        $this->invoiceNumber = $invoiceNumber;
+        return $this;
+    }
+
+    public function getStatus(): InvoiceStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(InvoiceStatus $status): Invoice
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): Invoice
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function addItem(InvoiceItem $item): Invoice
+    {
+        $item->setInvoice($this);
+
+        $this->items->add($item);
+
+        return $this;
+    }
+
+}
